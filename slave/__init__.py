7d640dac3311a4fc9eb2c5958deb3f93520535ab
Marco Halder <marco.halder@frm2.tum.de>
2012-08-02 16:26:33 +0200

    Added a check for ipythons autocall feature, since it can cause hard to find bugs.

--- slave/__init__.py
+++ slave/__init__.py
@@ -1,3 +1,18 @@
 #  -*- coding: utf-8 -*-
 #
 # Slave, (c) 2012, see AUTHORS.  Licensed under the GNU GPL.
+
+import warnings
+
+# Test if ipythons autocall feature is enabled. It can cause serious problems,
+# because attributes are executed twice. This means commands are send twice and
+# e.g. error flags might get cleared.
+try:
+    if __IPYTHON__.rc.autocall == 1:
+        warnings.warn('Autocall is enabled. Correct execution can not be '
+                      'guaranteed. To turn it off call ipython with '
+                      '-autocall 0.')
+except NameError:
+    pass
+
+del warnings
