b0b34dbc464391f70950f7ca15155fdfdfd9a444
Marco Halder <marco.halder@frm2.tum.de>
2012-11-08 14:14:21 +0100

    Implemented several LS370 commands.

--- slave/ls370.py
+++ slave/ls370.py
@@ -10,9 +10,43 @@ from slave.iec60488 import IEC60488
 from slave.types import Boolean, Enum, Float, Integer, Register, Set, String
 
 
+class Display(InstrumentBase):
+    """A LS370 Display at the chosen location.
+
+    :param connection: A connection object.
+    :param location: The display location.
+
+    :ivar config: The configuration of the display.
+        *(<channel>, <source>, <resolution>)*, where
+
+        * *<channel>* The index of the displayed channel, 0-16, where 0
+          activates channel scanning.
+        * *<source>* The displayed data. Valid entries are 'kelvin', 'ohm',
+          'linear', 'min' and 'max'
+        * *<resolution>* The displayed resolution in number of digits, 4-6.
+
+    """
+
+    def __init__(self, connection, location):
+        super(Display, self).__init__(connection)
+        location = int(location)
+        self.config = Command(
+            'DISPLOC? {0}'.format(location),
+            'DISPLOC {0},'.format(location),
+            [
+                Integer(min=0, max=16), 
+                Enum('kelvin', 'ohm', 'linear', 'min', 'max', start=1),
+                Integer(min=4, max=6)
+            ]
+        )
+
+
 class Heater(InstrumentBase):
     """An LS370 Heater.
 
+    :param manual_output: The manual heater output, a float representing the
+        percent of current or actual power depending on the heater output
+        selection.
     :param output: The heater output in percent of current or actual power
         dependant on the heater output selection.
     :param range: The heater current range. Valid entries are 'off',
@@ -21,8 +55,15 @@ class Heater(InstrumentBase):
     :param status: The heater status, either 'no error' or 'heater open error'.
 
     """
+    #: The supported heater ranges.
+    RANGE = [
+        'off', '31.6 uA', '100 uA', '316 uA', '1 mA',
+        '3.16 mA', '10 mA', '31.6 mA', '100 mA'
+    ]
+
     def __init__(self, connection):
         super(Heater, self).__init__(connection)
+        self.manual_output = Command('MOUT?', 'MOUT', Float)
         self.output = Command(('HTR?', Float))
         self.range = Command(
             'HTRRNG?',
@@ -33,6 +74,131 @@ class Heater(InstrumentBase):
         self.status = Command(
             ('HTRST?', Enum('no error', 'heater open error'))
         )
+
+
+class Input(InstrumentBase):
+    """A LS370 input channel.
+
+    :param connection: A connection object.
+    :param idx: The channel index.
+
+    :ivar alarm: The alarm configuration.
+        *(<enabled>, <source>, <high>, <low>, <deadband>, <latch>)*, where
+
+        * *<enable>* enables/disables the alarm, valid are `True`, `False`.
+        * *<source>* The data channel against which the alarm condition is
+          checked. Either 'kelvin', 'ohm' or 'linear'.
+        * *<high>* The high alarm value.
+        * *<low>* The low alarm value.
+        * *<deadband>* The value the source value must change to deactivate
+          the non-latched alarm.
+        * *<latch>* Enables/disables the latched alarm. (A latched alarm stays
+          active, even if the alarm condition isn't met anymore).
+
+    :ivar alarm_status: The status of the high and low alarm.
+        *(<high state>, <low state>)*, where
+
+        * *<high state>* is either `True`or `False`.
+        * *<low state>* is either `True`or `False`.
+
+    :ivar config: The input channel configuration.
+        *(<enabled>, <dwell>, <pause>, <curve>, <coefficient>)*, where
+
+        * *<enabled>* enables/disables the channel, valid are `True`, `False`.
+        * *<dwell>* The autoscanning dwell time in seconds, 1-200.
+        * *<pause>* The change pause time in seconds, 3-200.
+        * *<curve>* The curve used by the channel, valid are 'no curve' or
+          0-19 the index of the user curves.
+        * *<coefficient>* The temperature coefficient used if no curve is
+          selected. Valid are 'negative' and 'positive'.
+
+    :ivar excitation_power: The current excitation power.
+    :ivar filter: The filter parameters.
+        *(<enabled>, <settle time>, <window>)*, where
+
+        * *<enabled>* A boolean enabling/disabling the filtering.
+        * *<settle time>* The settle time in seconds, 1-200.
+        * *<window>* The filtering window, 1-80 in precent of the fullscale
+          reading.
+
+    :ivar index: The index of the input channel.
+    :ivar kelvin: The input channel reading in kelvin.
+
+        .. note:: If no curve is present, the reading will be `0.`.
+
+    :ivar linear: Linear equation data.
+    :ivar linear_equation: The input linear equation parameters.
+        *(<equation>, <m>, <x source>, <b source>, <b>)*, where
+
+         * *<equation>* is either `'slope-intercept'` or `'point-slope'`,
+           meaning 'y = mx + b' or 'y = m(x + b)'.
+         * *<m>* The slope.
+         * *<x source>* The input data to use, either 'kelvin', 'celsius' or
+           'sensor units'.
+         * *<b source>* Either 'value', '+sp1', '-sp1', '+sp2' or '-sp2'.
+         * *<b>* The b value if *<b source>* is set to 'value'.
+
+    :ivar minmax: The min max data, *(<min>, <max>)*, where
+
+         * *<min>* Is the minimum input data.
+         * *<max>* Is the maximum input data.
+
+    :ivar minmax_param: Configures the source data to use with the minmax
+        filter. Valid are 'kelvin', 'ohm' and 'linear'.
+    :ivar resistance: The input reading in ohm.
+
+    """
+    def __init__(self, connection, idx):
+        self.index = idx = int(idx)
+        self.alarm = Command(
+            'ALARM ? {0}'.format(idx),
+            'ALARM {0},'.format(idx),
+            [Boolean, Enum('kelvin', 'ohm', 'linear'),
+             Float, Float, Float, Boolean]
+        )
+        self.alarm_status = Command(
+            ('ALARMST? {0}'.format(idx), [Boolean, Boolean])
+        )
+        self.config = Command(
+            'INSET? {0}'.format(idx),
+            'INSET {0},'.format(idx),
+            [Boolean, Integer(min=1, max=200), Integer(min=3, max=200),
+             Enum('no curve', *range(20)), Enum('negative', 'positive', start=1)]
+        )
+        self.excitation_power = Command(('RDGPWR? {0}'.format(idx), Float))
+        self.filter = Command(
+            'FILTER? {0}'.format(idx)
+            'FILTER {0},'.format(idx),
+            [Boolean, Integer(min=1, max=200), Integer(min=1, max=80)]
+        )
+        self.kelvin = Command(('RDGK? {0}'.format(idx))
+        self.linear = Command(('LDAT? {0}'.format(idx), Float))
+        leq = [
+            Enum('slope-intercept', 'point-slope'),
+            Float,  # m value
+            Enum('kelvin', 'celsius', 'sensor units', start=1),
+            Enum('value', '+sp1', '-sp1', '+sp2', '-sp2', start=1),
+            Float,  # b value
+        ]
+        self.linear_equation = Command(
+            'LINEAR? {0}'.format(name),
+            'LINEAR {0},'.format(name),
+            [
+                Enum('slope-intercept', 'point-slope'),
+                Float,  # m value
+                Enum('kelvin', 'celsius', 'sensor units', start=1),
+                Enum('value', '+sp1', '-sp1', '+sp2', '-sp2', start=1),
+                Float,  # b value
+            ]
+        )
+        self.minmax = Command(('MDAT? {0}'.format(name), [Float, Float]))
+        self.minmax_parameter = Command(
+            'MNMX? {0}'.format(name),
+            'MNMX {0},'.format(name),
+            Enum('kelvin', 'ohm', 'linear'),
+        )
+        self.resistance = Command(('RDGR?', Float))
+
 
 class Output(InstrumentBase):
     """Represents a LS370 analog output.
@@ -73,6 +239,41 @@ class Output(InstrumentBase):
                                Float, Float, Float])
         self.value = Command(('AOUT? {0}'.format(channel), Float))
 
+
+class Relay(InstrumentBase):
+    """A LS370 relay.
+
+    :param connection: A connection object.
+    :param idx: The relay index.
+
+    :ivar config: The relay configuration.
+        *(<mode>, <channel>, <alarm>)*, where
+
+        * *<mode>* The relay mode either 'off' 'on', 'alarm' or 'zone'.
+        * *<channel>* Specifies the channel, which alarm triggers the relay.
+            Valid entries are 'scan' or an integer in the range 1-16.
+        * *<alarm>* The alarm type triggering the relay. Valid are 'low' 'high'
+            or 'both'.
+
+    :ivar status: The relay status.
+
+    """
+
+    def __init__(self, connection, idx):
+        super(Relay, self).__init__(connection)
+        idx = int(idx)
+        self.config = Command(
+            'RELAY? {0}'.format(idx),
+            'RELAY {0},'.format(idx),
+            [
+                Enum('off', 'on', 'alarm', 'zone'),
+                Enum('scan', range(1, 17)),
+                Enum('low', 'high', 'both')
+            ]
+        )
+        self.status = Command(('RELAYST? {0}'.format(idx), Boolean))
+
+# TODO CHGALL, CRVDEL, CRVHDR, CRVPT, 
 class LS370(IEC60488):
     """A lakeshore mode ls370 resistance bridge.
 
@@ -80,6 +281,8 @@ class LS370(IEC60488):
 
     :param connection: A connection object.
 
+    :ivar baud: The baud rate of the rs232 interface. Valid entries are `300`,
+        `1200` and `9600`.
     :ivar beeper: A boolean value representing the beeper mode. `True` means
         enabled, `False` means disabled.
     :ivar brightness: The brightness of the frontpanel display in percent.
@@ -87,8 +290,25 @@ class LS370(IEC60488):
     :ivar common_mode_reduction: The state of the common mode reduction.
     :ivar control_mode: The temperature control mode, valid entries are
         'closed', 'zone', 'open' and 'off'.
+    :ivar control_params: The temperature control parameters. *(<channel>,*
+        *<filter>, <units>, <delay>, <output>, <limit>, <resistance>)*, where
+
+        * *<channel>* The input channel used for temperature control.
+        * *<filter>* The filter mode, either 'filtered' or 'unfiltered'.
+        * *<units>* The setpoint units, either 'kelvin' or 'ohm'.
+        * *<delay>* The delay in seconds used for the setpoint change during
+          autoscan. An integer between 1 and 255.
+        * *<output>* The heater output display, either 'current' or 'power'.
+        * *<limit>* The maximum heater range. See :attr:`.Heater.RANGE`.
+        * *<resistance>* The heater load in ohms. Valid entries are 1. to
+          100000.
+
     :ivar digital_output: A register enabling/disabling the digital output
         lines.
+    :ivar displays: A tuple of :class:`.Display` instances, representing the 7
+        available display locations.
+    :ivar display_locations: The number of displayed locations, between 1 and
+        8.
     :ivar frequency: The excitation frequency. Valid entries are '9.8 Hz',
         '13.7 Hz' and '16.2 Hz'.
 
@@ -106,14 +326,35 @@ class LS370(IEC60488):
 
     :ivar mode: Represents the interface mode. Valid entries are
         `"local"`, `"remote"`, `"lockout"`.
-    :ivar output1: First output channel.
-    :ivar output2: Second output channel.
+    :ivar monitor: The monitor output selection, one of 'off', 'cs neg',
+        'cs pos', 'vad', 'vcm neg', 'vcm pos', 'vdif' or 'vmix'.
+    :ivar output: A tuple, holding two :class:`.Output` objects
+    :ivar pid: The pid loop settings. *(<p>, <i>, <d>)*, where
+
+        * *<p>* The proportional gain, a float in the range 0.001 to 1000.
+        * *<i>* The integral action, a float in the range 0 to 10000.
+        * *<d>* The derivative action, a float in the range 0 to 2500.
+
     :ivar polarity: The polarity of the temperature control. Valid entries are
         'unipolar' and 'bipolar'.
+    :ivar ramp: The setpoint ramping parameters. *(<enabled>, <rate>)*, where
+
+        * *<enabled>* Is a boolean, enabling/disabling the ramping.
+        * *<rate>* A float representing the ramping rate in kelvin per minute
+          in the range 0.001 to 10.
+    :ivar ramping: The ramping status, either `True` or `False`.
+    :ivar setpoint: The temperature control setpoint.
+    :ivar still: The still output value.
+
+        .. note::
+
+            The still only works, if it's properly configured in the analog
+            output 2.
 
     """
     def __init__(self, connection):
         super(LS370, self).__init__(connection)
+        self.baud = Command('BAUD?', 'BAUD', Enum(300, 1200, 9600))
         self.beeper = Command('BEEP?', 'BEEP', Boolean)
         self.brightness = Command('BRIGT?', 'BRIGT', Enum(25, 50, 75, 100))
         self.common_mode_reduction = Command('CMR?', 'CMR', Boolean)
@@ -122,11 +363,25 @@ class LS370(IEC60488):
             'CMODE',
             Enum('closed', 'zone', 'open', 'off', start=1)
         )
+        self.control_params = Command(
+            'CSET?',
+            'CSET',
+            [Integer(min=0, max=16), Enum('unfiltered', 'filtered'),
+             Enum('kelvin', 'ohm'), Integer(min=0, max=255), 
+             Enum('current', 'power', start=1), Enum(*Heater.RANGE),
+             Float(min=1., max=100000.)]
+        )
         # TODO disable if 3716 scanner option is used.
         self.digital_output = Command(
             'DOUT?',
             'DOUT',
             Register({'DO1': 0, 'DO2': 1, 'DO3': 2, 'DO4': 3, 'DO5': 4}),
+        )
+        self.displays = tuple(Display(connection, i) for i in range(1, 9))
+        self.display_locations = Command(
+            'DISPLAY?',
+            'DISPLAY',
+            Integer(min=1, max=8)
         )
         self.frequency = Command(
             'FREQ?',
@@ -140,9 +395,28 @@ class LS370(IEC60488):
                              Integer(min=0, max=30)])
         self.mode = Command('MODE?', 'MODE',
                             Enum('local', 'remote', 'lockout', start=1))
-        self.output1 = Output(connection, 1)
-        self.output2 = Output(connection, 2)
+        self.monitor = Command(
+            'MONITOR?',
+            'MONITOR', 
+            Enum('off', 'cs neg', 'cs pos', 'vad',
+                 'vcm neg', 'vcm pos', 'vdif', 'vmix')
+        )
+        self.output = (Output(connection, 1), Output(connection, 2))
+        self.pid = Command(
+            'PID?',
+            'PID',
+            [Float(min=1e-3, max=1e3), Float(min=0., max=1e4),
+             Float(min=0, max=2.5e3)]
+        )
         self.polarity = Command('CPOL?', 'CPOL', Enum('unipolar', 'bipolar'))
+        self.ramp = Command(
+            'RAMP?',
+            'RAMP',
+            [Boolean, Float(min=1e-3, max=10.)]
+        )
+        self.ramping = Command(('RAMPST?', Boolean))
+        self.setpoint = Command('SETP?', 'SETP', Float)
+        self.still = Command('STILL?', 'STILL', Float)
 
     def clear_alarm(self):
         """Clears the alarm status for all inputs."""
