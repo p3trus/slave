cde080f05cafc615df0f71f853aa61b9eebb2cf0
Marco Halder <marco.halder@frm2.tum.de>
2012-11-09 21:06:30 +0100

    Improved ForwardSequence implementation.

--- slave/misc.py
+++ slave/misc.py
@@ -2,7 +2,44 @@
 #
 # Slave, (c) 2012, see AUTHORS.  Licensed under the GNU GPL.
 
+import collections
 import threading
+
+
+class ForwardSequence(collections.Sequence):
+    """Sequence forwarding item access and write operations.
+
+    :param iterable: An iterable of items to be stored.
+    :param get: A callable used on item access, receiving the item.
+        It's result is returned.
+    :param set: A callable receiving the item and a value on item set
+        operations.
+
+    Implements a immutable sequence, which forwards item access and write
+    operations to the stored items.
+
+    """
+    def __init__(self, iterable, get, set=None):
+        super(ForwardSequence, self).__init__()
+        self._sequence = tuple(iterable)
+        self._get = get
+        self._set = set
+
+    def __len__(self):
+        return len(self._sequence)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return map(self._get, self._sequence[item])
+        return self._get(self._sequence[item])
+
+    def __setitem__(self, item, value):
+        if not self._set:
+            raise RuntimeError('Item not settable')
+        if isinstance(item, slice):
+            for i in self._sequence[item]:
+                self._set(i, value)
+        return self._set(item, value)
 
 
 class LockedConnection(object):
