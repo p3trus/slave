e0dc6e42c5de4f0090b02c9589d583b8635ea7cd
Marco Halder <marco.halder@frm2.tum.de>
2012-11-15 11:19:12 +0100

    Implemented frequency sweep mode command; Updated documentation.

--- slave/sr850.py
+++ slave/sr850.py
@@ -12,21 +12,30 @@ class SR850(InstrumentBase):
 
     .. rubric:: Reference and Phase Commands
 
-    :ivar reference_phase: The reference phase shift. A float between -360. and
+    :ivar phase: The reference phase shift. A float between -360. and
         719.999 in degree.
-    :ivar reference_source: The reference source, either 'internal', 'sweep' or
-        'external'.
+    :ivar reference_mode: The reference source mode, either 'internal', 'sweep'
+        or 'external'.
+    :ivar frequency: The reference frequency in Hz. A float between 0.001 and
+        102000.
+
+        .. note::
+
+            For harmonics greater than 1, the sr850 limits the max frequency to
+            102kHz / harmonic.
+
+    :ivar frequency_sweep: The type of the frequency sweep, either 'linear' or
+        'log', when in 'internal' reference_mode.
 
     """
     def __init__(self, connection):
         super(SR850, self).__init__(connection)
-        self.refrence_phase = Command(
-            'PHAS?',
-            'PHAS',
-            Float(min=-360., max=719.999)
+        self.phase = Command('PHAS?', 'PHAS', Float(min=-360., max=719.999)
         )
-        self.reference_source = Command(
+        self.reference_mode = Command(
             'FMOD?',
             'FMOD',
             Enum('internal', 'sweep', 'external')
         )
+        self.frequency = Command('FREQ?', 'FREQ', Float(min=0.001, max=102000))
+        self.frequency_sweep = Command('SWPT?', 'SWPT', Enum('linear', 'log'))
