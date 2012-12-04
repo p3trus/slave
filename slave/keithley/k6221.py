f6ca1508624ee3eb47895b184709d9e90a254bfc
Marco Halder <marco.halder@frm2.tum.de>
2012-12-04 11:51:00 +0100

    Implemented all Output related commands.

--- slave/keithley/k6221.py
+++ slave/keithley/k6221.py
@@ -1,6 +1,11 @@
+#  -*- coding: utf-8 -*-
+#
+# E21, (c) 2012, see AUTHORS.  Licensed under the GNU GPL.
+
 from slave.core import Command, InstrumentBase
 from slave.iec60488 import (IEC60488, Trigger, ObjectIdentification,
     StoredSetting)
+from slave.types import Enum, Mapping
 
 
 class Output(InstrumentBase):
@@ -8,22 +13,47 @@ class Output(InstrumentBase):
 
     :param connection: A connection object.
 
+    :ivar inner_shield: The inner shield connection, either 'output low' or
+        'guard'.
     :ivar interlock: Represents the state of the interlock, either 'open' or
         'closed'.
-    :ivar response: The output response mode, either 'fast' or 'slow'
+    :ivar output_low: The output low connection, either 'float' or 'ground'.
+    :ivar response: The output response mode, either 'fast' or 'slow'.
 
     """
     def __init__(self, connection):
         super(Output, self).__init__(connection)
+        self.inner_shield = Command(
+            'OUTP:ISH?',
+            'OUTP:ISH',
+            Mapping({'output low': 'OLOW', 'guard': 'GUAR'})
+        )
         self.interlock = Command((
             'OUTP:INT:TRIP?',
             Enum('open', 'closed')
         ))
+        self.output_low = Command(
+            'OUTP:LTE?',
+            'OUTP:LTE',
+            Enum('float', 'ground')
+        )
         self.response = Command(
             'OUTP:RESP?',
             'OUTP:RESP',
             Mapping({'fast': 'FAST', 'slow': 'SLOW'})
         )
+        self.status = Command((
+            'OUTP?',
+            Enum('disabled', 'enabled')
+        ))
+
+    def enable(self):
+        """Enables the output."""
+        self.connection.write('OUTP 1')
+
+    def disable(self):
+        """Disables the output."""
+        self.connection.write('OUTP 0')
 
 
 class K6221(IEC60488, Trigger, ObjectIdentification, StoredSetting):
@@ -37,3 +67,4 @@ class K6221(IEC60488, Trigger, ObjectIdentification, StoredSetting):
     def __init__(self, connection):
         super(K6221, self).__init__(connection)
         self.output = Output(connection)
+
