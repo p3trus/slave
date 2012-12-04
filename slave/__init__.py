f591e139424f09decf292e55a6db2406b9e50741
Marco Halder <marco.halder@frm2.tum.de>
2012-12-04 09:55:06 +0100

    Bumped version number to 0.3.0.dev

--- slave/__init__.py
+++ slave/__init__.py
@@ -5,7 +5,7 @@
 import warnings
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0.dev'
 
 # Test if ipythons autocall feature is enabled. It can cause serious problems,
 # because attributes are executed twice. This means commands are send twice and
