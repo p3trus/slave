31dee02eb2d263d86427b3eb72786510cbbe49ff
Marco Halder <marco.halder@frm2.tum.de>
2013-01-07 09:16:04 +0100

    Added context manager interface to TCPIPDevice, UsbtmcDevice and GpibDevice.

--- slave/connection.py
+++ slave/connection.py
@@ -16,20 +16,21 @@ box. One huge disadvantage of visa, not pyvisa, is the lacking support for
 modern linux kernels (>2.6). Alternatives do exist, e.g. :mod:`pyserial` can be
 used to interface with serial devices, The :mod:`socket` module allows network
 communication, linux has native support for usbtmc devices and linux-gpib can
-be used to talk to gpib devices. But they lack a consistent interface.
+be used to talk to gpib devices. But they all lack a consistent interface. To
+overcome this issue, a consistent interface to all of them is implemented in
+this module.
 
 E.g::
 
     from slave.connection import UsbtmcDevice
 
-    # connect to a tektronix tds2012C mountet at `/dev/usbtmc0`.
-    TDS2012C = UsbtmcDevice(0)
-
-    # emit clear command.
-    tds.write('*CLS')
-
-    # query status byte.
-    print tds.ask('*STB?')
+    # connect to a tektronix tds2012C mountet at `/dev/usbtmc0` using it's
+    # ContextManager interface.
+    with UsbtmcDevice(0) as TDS2012C:
+        # emit clear command.
+        tds.write('*CLS')
+        # query status byte.
+        print tds.ask('*STB?')
 
 """
 import threading
@@ -112,7 +113,19 @@ class GpibDevice(Connection):
 
     :param primary: The primary gpib address in the range of 0 to 30.
     :param secondary: The secondary gpib address.
-    :param board: The gpib board index.
+    :param board: The gpib board index. Linux-gpib supports up to 16 boards.
+    :param timeout: The timeout in seconds.
+
+    Usage::
+
+        from slave.connection import GpibDevice
+
+
+        # Connecting to a gpib device at address 8, using the context manager
+        # interface.
+        with GpibDevice(8) as connection:
+            # print the identification string of the device.
+            print connection.ask('*IDN?')
 
     """
     def __init__(self, primary=0, secondary=0, board=0, timeout=13, send_eoi=1,
@@ -136,10 +149,21 @@ class GpibDevice(Connection):
         # TODO allow customizable term char
         self._term_chars = '\n'
 
-    def __del__(self):
-        # XXX Should we catch exceptions here? ctypes might not be available in
-        # some cases due to the import system shutting down.
-        # Alternatively we could use 0 directly instead of ct.c_int(0).
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.close()
+
+    def close(self):
+        """Closes the gpib connection.
+
+        .. note::
+
+            Instead of directly calling `close()`, use it's ContextManager
+            interface.
+
+        """
         self._lib.ibonl(self._device, ct.c_int(0))
 
     def __write__(self, value):
@@ -161,6 +185,23 @@ class GpibDevice(Connection):
 
 
 class TCPIPDevice(Connection):
+    """A tiny wrapper for a socket connection.
+
+    :param address: The ip address as string.
+    :param port: The port.
+
+    Usage::
+
+        from slave.connection import TCPIPDevice
+
+
+        # Connecting to a tcpip device at address 168.178.0.1:1337, using the
+        # context manager interface.
+        with TCPIPDevice('168.178.0.1',1337) as connection:
+            # print the identification string of the device.
+            print connection.ask('*IDN?')
+
+    """
     def __init__(self, address, port):
         super(TCPIPDevice, self).__init__(
             # TODO convert dns address to ip
@@ -170,7 +211,21 @@ class TCPIPDevice(Connection):
         self._device.connect((address, port))
         self._term_chars = '\n'
 
-    def __del__(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.close()
+
+    def close(self):
+        """Closes the socket connection.
+
+        .. note::
+
+            Instead of directly calling `close()`, use it's ContextManager
+            interface.
+
+        """
         self._device.close()
 
     def __read__(self, bytes=1024):
@@ -193,6 +248,17 @@ class UsbtmcDevice(Connection):
         * Read and write access to the device descriptors, e.g. '/dev/usbtmc0'
           is required.
 
+    Usage::
+
+        from slave.connection import UsbtmcDevice
+
+
+        # Connecting to a usbtmc device at '/dev/usbtmc0', using the
+        # context manager interface.
+        with UsbtmcDevice(0) as connection:
+            # print the identification string of the device.
+            print connection.ask('*IDN?')
+
     """
     def __init__(self, primary):
         super(UsbtmcDevice, self).__init__(
@@ -201,7 +267,21 @@ class UsbtmcDevice(Connection):
         self._device = os.open('/dev/usbtmc{0}'.format(primary), os.O_RDWR)
         self._term_chars = '\n'
 
-    def __del__(self):
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        self.close()
+
+    def close(self):
+        """Closes the Usbtmc connection.
+
+        .. note::
+
+            Instead of directly calling `close()`, use it's ContextManager
+            interface.
+
+        """
         os.close(self._device)
 
     def __read__(self, bytes=1024):
