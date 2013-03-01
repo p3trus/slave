e39ee85b45048c57c6c9939753bc8f5f9e474ba6
p3trus <marco.halder@frm2.tum.de>
2013-03-01 11:51:17 +0100

    Changed ppms example code to make it more realistic.

--- examples/magnetotransport.py
+++ examples/magnetotransport.py
@@ -12,24 +12,24 @@ from slave.misc import Measurement
 
 # Connect to the lockin amplifier and the ppms
 lockin = SR830(visa.instrument('GPIB::10'))
-ppms = PPMS(visa.instrument('GPIB::15'))
+ppms = PPMS(visa.instrument('GPIB::15', delay=0.1, term_chars=';'))
 
 try:
     # Configure the lockin amplifier
-    lockin.frequency = 22.45  # Use a detection frequency of 22.45 Hz
+    lockin.frequency = 22.08  # Use a detection frequency of 22.08 Hz
     lockin.amplitude = 5.0    # and an amplitude of 5 V.
     lockin.reserve = 'low'
     lockin.time_constant = 3
 
     # Set the ppms temperature to 10 K, cooling with a rate of 3 K per min.
-    ppms.set_temperature(10, 3, wait_for_stability=True)
+    ppms.set_temperature(10, 20, wait_for_stability=True)
     # Now sweep slowly to avoid temperature instabilities.
     ppms.set_temperature(1.2, 0.5, wait_for_stability=True)
-    # Set a magnetic field of 1 T at a rate of 0.2 T per minute and set the magnet
+    # Set a magnetic field of 1 T at a rate of 150 mT per second and set the magnet
     # in persistent mode.
     #
     # Note: The PPMS uses Oersted instead of Tesla. 1 Oe = 0.1 mT.
-    ppms.set_field(10000, 2000, mode='persistent', wait_for_stability=True)
+    ppms.set_field(10000, 150, mode='persistent', wait_for_stability=True)
 
     # Set the appropriate gain. (We're assuming the measured voltage decreases
     # with increasing temperature.
