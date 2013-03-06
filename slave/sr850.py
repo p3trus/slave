1af5445579ef4d96838346b60218dac13cf46e9d
p3trus <marco.halder@frm2.tum.de>
2013-03-06 09:05:45 +0100

    Added Display and Scale Commands; Added Trace and Scan commands;

--- slave/sr850.py
+++ slave/sr850.py
@@ -1,7 +1,7 @@
 #  -*- coding: utf-8 -*-
 #
 # Slave, (c) 2012, see AUTHORS.  Licensed under the GNU GPL.
-from slave.core import Command, InstrumentBase
+from slave.core import Command, InstrumentBase, CommandSequence
 from slave.types import Enum, Float, Integer
 
 
@@ -97,6 +97,57 @@ class SR850(InstrumentBase):
          * <offset> the offset in percent, in the range -105.0 to 105.0.
          * <expand> the expand in the range 1 to 256.
 
+    .. rubric:: Trace and Scan Commands
+
+    :ivar traces: A sequence of four traces represented by the following tuple
+        *(<a>, <b>, <c>, <store>)* where 
+
+        * *<a>, <b>, <c>* define the trace which is calculated as 
+          *<a> * <b> / <c>*. Each one of them is one of the following
+          quantities '1', 'x', 'y', 'r', 'theta', 'xn', 'yn', 'rn', 'Al1',
+          'Al2', 'Al3', 'Al4', 'F', 'x**2', 'y**2', 'r**2', 'theta**2',
+          'xn**2', 'yn**2', 'rn**2', 'Al1**2', 'Al2**2', 'Al3**2', 'Al4**2' or
+          'F**2'
+        * *<store>* is a boolean defining if the trace is stored.
+
+    :ivar scan_sample_rate: The scan sample rate, valid are 62.5e-3, 125e-3,
+        250e-3, 500e-3, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 in Hz or
+        'trigger'.
+
+    :ivar scan_length: The scan length in seconds. It well be set to the
+        closest possible time. The minimal scan time is 1.0 seconds. The
+        maximal scan time is determined by the scanning sample rate and the
+        number of stored traces.
+
+        ======  ===========
+        Traces  Buffer Size
+        ======  ===========
+        1       64000
+        2       32000
+        3       48000
+        4       16000
+        ======  ===========
+
+    :ivar scan_mode: The scan mode, valid are 'shot' or 'loop'.
+
+    .. rubric:: Display and Scale Commands
+
+    :ivar active_display: Selects the active display. Valid are 'full', 'top'
+        or 'bottom'.
+    :ivar screen_format: Selects the screen format. Valid are
+
+        * 'single', The complete screen is used.
+        * 'dual', A up/down split view is used.
+
+    :ivar monitor_display: The monitor display mode, valid are 'settings' and
+        'input/output'.
+    :ivar full_display: An instance of :class:`~.Display`, representing the
+        full display.
+    :ivar top_display: An instance of :class:`~.Display`, representing the
+        top display.
+    :ivar bottom_display: An instance of :class:`~.Display`, representing the
+        bottom display.
+
     """
     def __init__(self, connection):
         super(SR850, self).__init__(connection)
@@ -150,7 +201,7 @@ class SR850(InstrumentBase):
             'SENS?',
             'SENS',
             Enum(2e-9, 5e-9, 10e-9, 20e-9, 50e-9, 100e-9, 200e-9, 500e-9, 1e-6,
-                 2e-6, 5e-6, 10e-6, 20e-6, 50e-6, 100e-6, 200e-6, 500e-6, 1e-3
+                 2e-6, 5e-6, 10e-6, 20e-6, 50e-6, 100e-6, 200e-6, 500e-6, 1e-3,
                  2e-3, 5e-3, 10e-3, 20e-3, 50e-3, 100e-3, 200e-3, 500e-3, 1)
         )
         self.reserve_mode = Command(
@@ -168,7 +219,7 @@ class SR850(InstrumentBase):
         self.filter_slope = Command('OFSL?', 'OFSL', Enum(6, 12, 18, 24))
         self.syncronous_filtering = Command('SYNC?', 'SYNC', Boolean)
         # Output and Offset Commands
-        self.ch1_display = Command(    
+        self.ch1_display = Command(
             'FOUT? 1',
             'FOUT 1,',
             Enum('x', 'r', 'theta', 'trace1', 'trace2', 'trace3', 'trace4')
@@ -193,6 +244,53 @@ class SR850(InstrumentBase):
             'OEXP 3,',
             (Float(min=-105., max=105.), Integer(min=1, max=256))
         )
+        # Trace and Scan Commands
+        traces = []
+        quantities = Enum(
+            '1', 'x', 'y', 'r', 'theta', 'xn', 'yn', 'rn', 'Al1', 'Al2', 'Al3',
+            'Al4', 'F', 'x**2', 'y**2', 'r**2', 'theta**2', 'xn**2', 'yn**2',
+            'rn**2', 'Al1**2', 'Al2**2', 'Al3**2', 'Al4**2', 'F**2')
+        for i in range(1, 5):
+            cmd = Command(
+                'TRCD? {0}'.format(i),
+                'TRCD {0},'.format(i),
+                (quantities, quantities, quantities, Boolean)
+            )
+            traces.append(cmd)
+        self.traces = CommandSequence(traces)
+        self.scan_sample_rate = Command(
+            'SRAT?',
+            'SRAT',
+            Enum(62.5e-3, 125e-3, 250e-3, 500e-3, 1, 2, 4,
+                 8, 16, 32, 64, 128, 256, 512, 'trigger')
+        )
+        self.scan_length = Command('SLEN?', 'SLEN', Float(min=1))
+        self.scan_mode = Command('SEND?', 'SEND', Enum('shot', 'loop'))
+        # Display and Scale Commands
+        # TODO: ADSP, SMOD, MNTR, DTYP, DTRC, DSCL, DOFF
+        self.active_display = Command(
+            'ADSP?',
+            'ADSP',
+            Enum('full', 'top', 'bottom')
+        )
+        self.screen_format = Command(
+            'SMOD?',
+            'SMOD',
+            Enum('single', 'dual')
+        )
+        self.monitor_display = Command(
+            'MNTR?',
+            'MNTR',
+            Enum('settings', 'input/output')
+        )
+        self.full_display = Display(0, self.connection, self._cfg)
+        self.top_display = Display(1, self.connection, self._cfg)
+        self.bottom_display = Display(2, self.connection, self._cfg)
+        # Cursor Commands
+        # TODO: CSEK, CWID, CDIV, CLNK, CDSP, CMAX, CURS, CBIN,
+
+        # Mark Commands
+
 
     def auto_offset(quantity):
         """Automatically offsets the given quantity.
@@ -200,5 +298,77 @@ class SR850(InstrumentBase):
         :param quantity: The quantity to offset, either 'x', 'y' or 'r'
 
         """
-        self._write(('AOFF', Enum('x', 'y', 'r', start=1), quantity)
+        self._write(('AOFF', Enum('x', 'y', 'r', start=1), quantity))
 
+    def auto_scale(self):
+        """Autoscales the active display.
+
+        .. note:: Just Bar and Chart displays are affected.
+
+        """
+        self._write('ASCL')
+
+
+class Display(InstrumentBase):
+    """Represents a SR850 display.
+
+    .. note::
+
+        The SR850 will generate an error if one tries to set a parameter of an
+        invisible display.
+
+    :ivar type: The display type, either 'polar', 'blank', 'bar' or 'chart'.
+
+    :ivar trace: The trace number of the displayed trace.
+    :ivar range: The displayed range, a float between 10^-18 and 10^18.
+
+        .. note:: Only bar and chart displays are affected.
+
+    :ivar offset: The display center value in units of the trace in the range
+        10^-12 to 10^12.
+
+    :ivar horizontal_scale: The display's horizontal scale. Valid are '2 ms',
+        '5 ms', '10 ms', '20 ms', '50 ms', '0.1 s', '0.2 s', '0.5 s', '1 s',
+        '2 s', '5 s', '10 s', '20 s', '50 s', '1 min', '100 s', '2 min',
+        '200 s', '5 min', '500 s', '10 min', '1 ks', '20 min', '2 ks', '1 h',
+        '10 ks', '3 h', '20 ks', '50 ks', '100 ks' and '200 ks'.
+
+    :ivar bin: The bin number at the right edge of the chart display.
+        (read only)
+
+        .. note:: The selected display must be a chart display.
+
+    """
+    def __init__(self, idx, connection, cfg):
+        super(Display, self).__init__(connection, cfg)
+        idx = int(idx)
+        self.type = Command(
+            'DTYP? {0}'.format(idx),
+            'DTYP {0},'.format(idx),
+            Enum('polar', 'blank', 'bar', 'chart')
+        )
+        self.trace = Command(
+            'DTRC? {0}'.format(idx),
+            'DTRC {0},'.format(idx),
+            Integer(min=1, max=4)
+        )
+        self.range = Command(
+            'DSCL? {0}'.format(idx),
+            'DSCL {0},'.format(idx),
+            Float(min=1e-18, max=1e18)
+        )
+        self.offset = Command(
+            'DOFF? {0}'.format(idx),
+            'DOFF {0},'.format(idx),
+            Float(min=1e-12, max=1e12)
+        )
+        self.horizontal_scale = Command(
+            'DHZS? {0}'.format(idx),
+            'DHZS {0},'.format(idx),
+            Enum('2 ms', '5 ms', '10 ms', '20 ms', '50 ms', '0.1 s', '0.2 s',
+                 '0.5 s', '1 s', '2 s', '5 s', '10 s', '20 s', '50 s', '1 min',
+                 '100 s', '2 min', '200 s', '5 min', '500 s', '10 min', '1 ks',
+                 '20 min', '2 ks', '1 h', '10 ks', '3 h', '20 ks', '50 ks',
+                 '100 ks', '200 ks')
+        )
+        self.bin = Command(('RBIN ? {0}'.format(idx), Integer))
