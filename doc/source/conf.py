d9ef9fd69491b7ea3c38589bdfe990ed0406e2ed
p3trus <marco.halder@frm2.tum.de>
2014-11-24 17:14:01 +0100

    Mock out numpy so that autodoc builds.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,10 +12,18 @@
 # serve to show the default.
 
 import sys, os
-
 # Check if doc is build on readthedocs
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    from unittest.mock import MagicMock
 
+    class Mock(object):
+        @classmethod
+        def __getattr__(cls, name):
+            return Mock()
+
+    MOCK_MODULES = ['numpy']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
