7ad2af07e7d6feddb99618fac680696b2d5ec6fd
p3trus <marco.halder@frm2.tum.de>
2015-01-20 17:44:37 +0100

    Added LockinMeasurement helper. It can autorange the lockin amplifiers.

--- slave/misc.py
+++ slave/misc.py
@@ -4,10 +4,36 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from future.builtins import *
+import future.utils
 import csv
 import collections
 import threading
 import logging
+import os.path
+import io
+
+
+SI_PREFIX = {
+    'y': 1e-24,  # yocto
+    'z': 1e-21,  # zepto
+    'a': 1e-18,  # atto
+    'f': 1e-15,  # femto
+    'p': 1e-12,  # pico
+    'n': 1e-9,   # nano
+    'u': 1e-6,   # micro
+    'm': 1e-3,   # mili
+    'c': 1e-2,   # centi
+    'd': 1e-1,   # deci
+    '': 1.,
+    'k': 1e3,    # kilo
+    'M': 1e6,    # mega
+    'G': 1e9,    # giga
+    'T': 1e12,   # tera
+    'P': 1e15,   # peta
+    'E': 1e18,   # exa
+    'Z': 1e21,   # zetta
+    'Y': 1e24,   # yotta
+}
 
 
 class ForwardSequence(collections.Sequence):
@@ -67,12 +93,92 @@ def index(index, length):
     raise IndexError()
 
 
+def range_to_numeric(ranges):
+    """Converts a sequence of string ranges to a sequence of floats.
+
+    E.g.::
+
+        >>> range_to_numeric(['1 uV', '2 mV', '1 V'])
+        [1E-6, 0.002, 1.0]
+
+    """
+    values, units = zip(*(r.split() for r in ranges))
+    # Detect common unit.
+    unit = os.path.commonprefix([u[::-1] for u in units])
+
+    # Strip unit to get just the SI prefix.
+    prefixes = (u[:-len(unit)] for u in units)
+
+    # Convert string value and scale with prefix.
+    values = [float(v) * SI_PREFIX[p] for v, p in zip(values, prefixes)]
+    return values
+
+
+class AutoRange(object):
+    """Estimates an appropriate sensitivity range.
+
+    A mean value is calculated from the magnitude of the value and previous
+    ones(the number depends on the `buffer_len`). The best range is chosen
+    as the largest range, where the mean is smaller than `scale * range`. If
+    the mean is larger than any range, the largest range is returned.
+
+    :param range: A sequence of sensitivity ranges.
+    :param names: An optional sequence of names corresponding to the ranges. If
+        given, :meth:`AutoRange.range` returns the name instead of the range.
+    :param scale: An optional parameter scaling the ranges.
+    :param buffer_len: Defines the buffer length used to calculate the mean
+        value in :meth:`~.AutoRange.range`.
+
+    """
+    def __init__(self, ranges, names=None, scale=1., buffer_len=10):
+        if names:
+            if len(ranges) != len(names):
+                raise ValueError('Unequal length of names and ranges.')
+            self._mapping = {r:k for r, k in zip(ranges, names)}
+        else:
+            self._mapping = None
+        self.ranges = sorted(ranges)
+        self.scale = scale
+        self._buffer = collections.deque(maxlen=buffer_len)
+
+    def range(self, value):
+        """Estimates an appropriate sensitivity range."""
+        self._buffer.append(abs(value))
+        mean = sum(self._buffer) / len(self._buffer)
+        estimate = next(
+            (r for r in self.ranges if mean < self.scale * r),
+            self.ranges[-1]
+        )
+        if self._mapping:
+            return self._mapping[estimate]
+        else:
+            return estimate
+
+
 class Measurement(object):
     """Small measurement helper class.
 
     For each call to :meth:`.__call__` a comma separated row, representing the
     return values for each callable item in measurables is written to the file
     specified by path.
+    ::
+
+        >>> from slave.misc import Measurement
+        >>> names = ['A', 'B']
+        >>> measurables = [lambda: 'a', lambda: 'b']
+        >>> with Measurement('test.csv', measurables, names) as m:
+        ...     m()
+        ...     m()
+        ...
+        >>> with open('test.csv', 'r') as f:
+        ...     print(f.readlines())
+        ...
+        ['A,B\n', 'a,b\n', 'a,b\n']
+
+    :param path: The file path.
+    :param measurables: A sequence of callables.
+    :param names: An optional sequence of names, used to create the csv header.
+        The number of names and measurables must be equal.
 
     """
     def __init__(self, path, measurables, names=None):
@@ -85,15 +191,17 @@ class Measurement(object):
 
     def open(self):
         if not self._file:
-            self._file = open(self._path, 'wb')
-            self._writer = csv.writer(self._file)
+            if future.utils.PY3:
+                self._file = open(self._path, 'w', newline='')
+            else:
+                self._file = open(self._path, 'wb')
+            self._writer = csv.writer(self._file, lineterminator='\n')
             if self._names:
                 self._writer.writerow(self._names)
 
     def close(self):
         if self._file:
             self._file.close()
-            self._file = None
             self._writer = None
 
     def __call__(self):
@@ -104,3 +212,61 @@ class Measurement(object):
 
     def __exit__(self, type, value, traceback):
         self.close()
+
+
+class LockInMeasurement(Measurement):
+    """A measurement helper optimized for lock-in amplifier measurements.
+
+    E.g.::
+
+        ppms = PPMS(visa('GPIB::12'))
+        lia1 = SR7230(socket(address=('192.168.178.1', 50000)))
+        lia2 = SR7230(socket(address=('192.168.178.2', 50000)))
+
+        ppms.set_field(10000, 100)
+        ppms.set_temperature(2, 10)
+
+        env_params = [
+            lambda: ppms.temperature,
+            lambda: ppms.field,
+        ]
+        names = ['x1', 'y1', 'x2', 'y2', 'temperature', 'field']
+
+        with LockInMeasurement('data.csv', [lia1, lia2], env_params, names) as measure:
+            ppms.scan_temperature(measure, 300, 1)
+
+    :param path: The filepath.
+    :param lockins: A sequence of lockin drivers. A lockin driver must have a
+        readable `x` and `y` attribute to get the data. Additionally a readable
+        `SENSITIVITY` attribute and a read and writeable `sensitivity`
+        attribute are mandatory.
+    :param measurables: An optional sequence of functions.
+    :param names: A sequence of names used to generate the csv file header.
+    :param bool autorange: Enables/disables auto ranging.
+
+    """
+    def __init__(self, path, lockins, measurables=None, names=None, autorange=True):
+        super(LockInMeasurement, self).__init__(path, measurables or [], names=names)
+        self._lockins = lockins
+        self._autorange = []
+        if autorange:
+            for lia in lockins:
+                ranges, names = lia.SENSITIVITY, None
+                # Check if sensitivity ranges are already numeric or strings.
+                if isinstance(ranges[0], str):
+                    ranges, names = range_to_numeric(ranges), ranges
+                self._autorange.append(AutoRange(ranges, names))
+
+    def __call__(self):
+        lockin_xy = [(lia.x, lia.y) for lia in self._lockins]
+        optional_data = [m() for m in self._measurables]
+        # If autoranging is enabled,
+        if self._autorange:
+            for lia, auto, (x, y) in zip(self._lockins, self._autorange, lockin_xy):
+                sens = auto.range(max(x, y))
+                if lia.sensitivity != sens:
+                    lia.sensitivity = sens
+
+        # Flatten lockin data and concatenate with optional data.
+        data = [d for xy in lockin_xy for d in xy] + optional_data
+        self._writer.writerow(data)
--- slave/test/test_misc.py
+++ slave/test/test_misc.py
@@ -4,8 +4,10 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from future.builtins import *
+import os
 import pytest
-from slave.misc import index, ForwardSequence
+from slave.misc import (index, ForwardSequence, range_to_numeric, AutoRange,
+                        Measurement, LockInMeasurement)
 
 
 class TestIndex(object):
@@ -50,3 +52,110 @@ class TestForwardSequence(object):
         sequence = ForwardSequence([1, 2, 3], getter)
         with pytest.raises(RuntimeError):
             sequence[0] = 1
+
+
+def test_sens_to_numeric():
+    ranges = ['1 nOhm', '2 uOhm', '5 mOhm', '1 Ohm']
+    assert range_to_numeric(ranges) == [1e-9, 2e-6, 5e-3, 1.]
+
+
+class TestAutoRange(object):
+    def test_with_numeric_range(self):
+        auto = AutoRange([1e-6, 1e-3, 1.], scale=1., buffer_len=3)
+        assert auto.range(.9e-6) == 1e-6 # mean is .9e-6
+        assert auto.range(.9e-6) == 1e-6 # mean is .9e-6
+        assert auto.range(1.2e-6) == 1e-3 # mean is 1e-6; sens should switch.
+
+    def test_with_numeric_range_and_negative_values(self):
+        auto = AutoRange([1e-6, 1e-3, 1.], scale=1., buffer_len=3)
+        assert auto.range(.9e-6) == 1e-6 # mean is .9e-6
+        assert auto.range(-.9e-6) == 1e-6 # mean of magnitude is still .9e-6
+        assert auto.range(-1.2e-6) == 1e-3 # mean is 1e-6; sens should switch.
+
+    def test_with_out_of_range_value(self):
+        auto = AutoRange([1e-6, 1e-3, 1.], scale=1.)
+        assert auto.range(1.1) == 1.
+
+    def test_with_named_ranges(self):
+        auto = AutoRange([1e-6, 1e-3, 1.], ['1 uV', '1 mV', '1 V'],
+                         scale=1., buffer_len=3)
+        assert auto.range(.9e-6) == '1 uV' # mean is .9e-6
+        assert auto.range(-.9e-6) == '1 uV' # mean of magnitude is still .9e-6
+        assert auto.range(-1.2e-6) == '1 mV' # mean is 1e-6; sens should switch.
+
+    def test_with_unequal_length_of_ranges_and_names(self):
+        with pytest.raises(ValueError):
+            AutoRange([1e-6, 1e-3, 1], names=['1 mV', '1 V'])
+
+
+class TestMeasurement(object):
+    def test_calling(self, tmpdir):
+        path = tmpdir.join('data.csv')
+        params = [lambda: 1, lambda: 2]
+        with Measurement(str(path), params) as measure:
+            measure()
+        assert path.read() == '1,2\n'
+
+    def test_calling_with_names(self, tmpdir):
+        path = tmpdir.join('data.csv')
+        params = [lambda: 1, lambda: 2]
+        names = ['A', 'B']
+        with Measurement(str(path), params, names) as measure:
+            measure()
+        assert path.read() == 'A,B\n1,2\n'
+
+    def test_if_filehandle_is_closed_on_error(self, tmpdir):
+        path = tmpdir.join('data.csv')
+        params = [lambda: 1, lambda: 2]
+        try:
+            with Measurement(str(path), params) as measure:
+                measure()
+                raise ValueError()
+        except ValueError:
+            pass
+        finally:
+            assert measure._file.closed
+
+
+class MockLockIn(object):
+    def __init__(self, x, y, sensitivities):
+        self.x = x
+        self.y = y
+        self.SENSITIVITY = sensitivities
+        self.sensitivity = self.SENSITIVITY[0]
+
+
+class TestLockInMeasurement(object):
+    def test_without_autorange(self, tmpdir):
+        path = tmpdir.join('data.csv')
+        SENSITIVITY = [1e-6, 1e-3, 1.]
+        lockins = [MockLockIn(1.3, 1.4, SENSITIVITY)]
+        env_params = [lambda: 'env']
+        names = ['X1', 'Y1', 'ENV']
+        with LockInMeasurement(str(path), lockins, env_params, names, autorange=False) as measure:
+            measure()
+        assert path.read() == 'X1,Y1,ENV\n1.3,1.4,env\n'
+        # Sensitivity should not be changed.
+        assert lockins[0].sensitivity == 1e-6
+
+    def test_with_string_sensitivities(self, tmpdir):
+        path = tmpdir.join('data.csv')
+        SENSITIVITY = ['1 uV', '1 mV', '1 V']
+        lockins = [MockLockIn(1.3, 1.4, SENSITIVITY)]
+        env_params = [lambda: 'env']
+        names = ['X1', 'Y1', 'ENV']
+        with LockInMeasurement(str(path), lockins, env_params, names, autorange=True) as measure:
+            measure()
+        assert path.read() == 'X1,Y1,ENV\n1.3,1.4,env\n'
+        assert lockins[0].sensitivity == '1 V'
+
+    def test_with_numeric_sensitivities(self, tmpdir):
+        path = tmpdir.join('data.csv')
+        SENSITIVITY = [1e-6, 1e-3, 1.]
+        lockins = [MockLockIn(1.3, 1.4, SENSITIVITY)]
+        env_params = [lambda: 'env']
+        names = ['X1', 'Y1', 'ENV']
+        with LockInMeasurement(str(path), lockins, env_params, names, autorange=True) as measure:
+            measure()
+        assert path.read() == 'X1,Y1,ENV\n1.3,1.4,env\n'
+        assert lockins[0].sensitivity == 1.
